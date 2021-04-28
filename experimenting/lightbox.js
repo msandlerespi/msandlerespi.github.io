6f84cece8cd65c779f032688cd6169f7ffb3ad54
Miguel SE <masemmiguel@gmail.com>
2021-04-27 23:49:42 -0400

    fixed iframe size bug

--- experimenting/lightbox.js
+++ experimenting/lightbox.js
@@ -2,14 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("open").onclick = () => {
         document.getElementById("box").classList = "opened";
         setTimeout(() => { 
-            document.getElementById("video").classList = "opened";
+            let video = document.getElementById("video");
+            video.classList = "opened";
+            video.width = window.innerWidth/1.5;
+            video.height = window.innerHeight/1.6;
             document.getElementById("exit").classList += " opened";
         }, 1000);
         setTimeout(() => { document.getElementById("video").src += "?autoplay=1"; }, 1300);
     }
     document.getElementById("exit").onclick = () => {
-        document.getElementById("video").src = "https://www.youtube.com/embed/tgbNymZ7vqY";
-        document.getElementById("video").classList = "";
+        let video = document.getElementById("video");
+        video.width = 0;
+        video.height = 0;
+        video.src = "https://www.youtube.com/embed/tgbNymZ7vqY";
+        video.classList = "";
         document.getElementById("exit").classList = "fas fa-times";
         setTimeout(() => { document.getElementById("box").classList = ""; }, 300);
         
